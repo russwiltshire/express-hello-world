ea932735a8ddb2b12af8d5cc7b777651f57c4fbd
russwiltshire <russ.wiltshire@northcapesoftware.com>
2024-12-03 15:42:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,23 +7,40 @@ const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 const port = process.env.PORT || 3000;
 
+// env var containing the secret we use to sign the authentication cookies
+const authSecret = process.env.AUTH_SECRET || 3000;
+
+// configure the session
+app.use(session({
+  secret: 'authSecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: true,
+    maxAge: 30 * 60 * 1000
+  }
+}));
+
 // Middleware to parse JSON request bodies
 app.use(express.json());   
 
 app.use(cors()); // Enables CORS for all routes
 
+// example route that does authentication and signs in
 app.post('/api/auth/signin', (req, res) => {
   
   // Access the data sent in the POST request body (if any)
   // Should be username and password.
   const requestData = req.body;
 
-  // TODO: authentication logic ...
+  // Simulate user authentication (replace with your actual logic)
+  const user = { id: 123, username: 'bobross' };
   const authenticated = true;
-
+  
   // Send the JSON response
   if (authenticated) {
-      // TODO: store username in session ...
+      // Store user data in the session
+      req.session.user = user;
       // send the response
       res.status(200).json( { message: 'Login successful' } );
   } else {
@@ -31,6 +48,17 @@ app.post('/api/auth/signin', (req, res) => {
   }
 
 });
+
+
+// Example route that accesses session data
+app.get('/api/auth/getsession', (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome, ${req.session.user.username}!`);
+  } else {
+    res.send('You need to log in first.');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
